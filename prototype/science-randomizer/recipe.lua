2c2e7545431edd35042d362b5437d4446c13c587
Artem Betz <arbet100@uni-duesseldorf.de>
2023-08-27 18:36:48 +0200

    recipe adjusting

--- prototype/science-randomizer/recipe.lua
+++ prototype/science-randomizer/recipe.lua
@@ -1,4 +1,4 @@
-
+require("prototype.science-randomizer.prep.calc-stats")
 local recipes = require("prototype.science-randomizer.prep.build_recipe")
 print("hi")
 
@@ -6,5 +6,11 @@ for key, value in pairs(recipes) do
     local recipe = data.raw.recipe[key]
     recipe.ingredients = value.ingredients
     recipe.category = value.category
+    if recipe.result_count then
+        recipe["result_count"] = recipe.result_count * value.result_count_multiplier
+    else
+        recipe["result_count"] = value.result_count_multiplier
+    end
 end
+
 print("hi")
