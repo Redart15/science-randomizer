64e0f4b8e4126aa5a084b429ba8b3f2683c125e4
Artem Betz <arbet100@uni-duesseldorf.de>
2023-09-03 01:04:59 +0200

    new settings

--- settings.lua
+++ settings.lua
@@ -9,5 +9,57 @@ data:extend(
             minimum_value = 0,
             order = "aaaa"
         },
+        {
+            type = "bool-setting",
+            name = mod_id .. "allow-raw",
+            setting_type = "startup",
+            default_value = false,
+            order = "aaba"
+        },
+        {
+            type = "bool-setting",
+            name = mod_id .. "allow-fluid",
+            setting_type = "startup",
+            default_value = false,
+            order = "aabb"
+        },
+        {
+            type = "bool-setting",
+            name = mod_id .. "allow-science",
+            setting_type = "startup",
+            default_value = false,
+            order = "aabc"
+        },
+        {
+            type = "bool-setting",
+            name = mod_id .. "allow-grown",
+            setting_type = "startup",
+            default_value = false,
+            order = "aabd"
+        },
+        {
+            type = "bool-setting",
+            name = mod_id .. "balanced",
+            setting_type = "startup",
+            default_value = true,
+            order = "aabd"
+        },
+        {
+            type = "bool-setting",
+            name = mod_id .. "in-tier",
+            setting_type = "startup",
+            default_value = true,
+            order = "aabd"
+        },
+        {
+            type = "string-setting",
+            name = mod_id .. "set-packs",
+            setting_type = "startup",
+            default_value = "",
+            allow_blank = true,
+            allow_trim = true,
+            hidden = true,
+            order = "aaab"
+        },
     }
 )
