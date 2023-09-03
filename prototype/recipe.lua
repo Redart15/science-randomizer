cb02d6769b230e05f050fee947171feccd4d5bfc
Artem Betz <arbet100@uni-duesseldorf.de>
2023-09-03 11:11:22 +0200

    convert to recipe.results to be able to make them output maybe packs of packs in the future to balance the output

--- prototype/recipe.lua
+++ prototype/recipe.lua
@@ -1,6 +1,6 @@
 local recipes = require("prep.tier_to_prototypes")
 local dataRecipe = data.raw.recipe
-local calc_cost, calc_crafting_category
+local calc_cost, calc_crafting_category, build_results
 
 function calc_cost(ingredients)
     local sum = 0
@@ -8,6 +8,17 @@ function calc_cost(ingredients)
         sum = sum + item.amount * item.cost
     end
     return sum
+end
+
+function build_results(pack, recipe, count)
+    local temp = {}
+    local result = {
+        name = recipe.name,
+        amount = count,
+        type = "item"
+    }
+    table.insert(temp, result)
+    return temp
 end
 
 function calc_crafting_category(fluidCount)
@@ -24,8 +35,13 @@ for _, recipe in ipairs(recipes.item_list) do
     local pack = dataRecipe[recipe.name]
     pack.ingredients = recipe.recipe:to_list()
     pack.category = calc_crafting_category(recipe.fluidCount)
+    local count
     if recipes.settings.isBalanced == true then
         local cost = calc_cost(recipe.recipe.values)
-        pack.result_count = math.ceil(cost / recipe.cost)
+        count = math.ceil(cost / recipe.cost)
     end
+    pack.results = build_results(pack, recipe, count)
+    print("hi")
 end
+
+print(data.raw.recipe["automation-science-pack"])
