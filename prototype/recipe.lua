585d73695e14f9bd536b127c819e63d0cc3fce65
Artem Betz <arbet100@uni-duesseldorf.de>
2023-09-06 09:10:56 +0200

    removed util function, as they are now part of util them self, moved the recipe cost calculation futher back.

--- prototype/recipe.lua
+++ prototype/recipe.lua
@@ -1,11 +1,59 @@
+local util = require("libs.factorio.util")
 local recipes = require("prep.tier_to_prototypes")
+local lookup = require("prep.lookup")
 local dataRecipe = data.raw.recipe
-local calc_cost, calc_crafting_category, build_results
+local calc_costs,
+calc_crafting_category,
+build_results,
+calc_cost,
+get_count
 
-function calc_cost(ingredients)
+---comment
+---@param itemname any
+---@return integer
+function calc_cost(itemname)
+    local recipe = data.raw.recipe[itemname]
+    local ingredients = util.get_ingredients(recipe)
+    if next(ingredients) == nil then
+        return 3
+    end
+    local sum = 0
+    for _, ing in ipairs(ingredients) do
+        local cost = lookup["costs"][ing.name]
+        if cost == nil or cost == 0 then
+            cost = calc_cost(ing.name)
+        end
+        sum = sum + cost * ing.amount
+    end
+    local count = get_count(recipe)
+    local cost = math.ceil(sum / count)
+    lookup["costs"][itemname] = cost
+    return cost
+end
+
+---comment
+---@param recipe table
+---@return integer
+function get_count(recipe)
+    local count
+    if recipe.results ~= nil and next(recipe.results) ~= nil then
+        count = 0
+        local results = util.get_results(recipe)
+        for _, result in ipairs(results) do
+            count = count + result.amount
+        end
+        return count
+    end
+    if recipe.result_count ~= nil then
+        return recipe.result_count
+    end
+    return 1
+end
+
+function calc_costs(ingredients)
     local sum = 0
     for _, item in pairs(ingredients) do
-        sum = sum + item.amount * item.cost
+        sum = sum + item.amount * calc_cost(item.name)
     end
     return sum
 end
@@ -37,7 +85,7 @@ for _, recipe in ipairs(recipes.item_list) do
     pack.category = calc_crafting_category(recipe.fluidCount)
     local count
     if recipes.settings.isBalanced == true then
-        local cost = calc_cost(recipe.recipe.values)
+        local cost = calc_costs(recipe.recipe.values)
         count = math.ceil(cost / recipe.cost)
     end
     pack.results = build_results(pack, recipe, count)
