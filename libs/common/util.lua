e00451735f43a846133e439868bf203c2c0a6a7a
Artem Betz <arbet100@uni-duesseldorf.de>
2023-09-09 20:29:58 +0200

    added a button to export/imprt new data and as such freze recipes

--- libs/common/util.lua
+++ libs/common/util.lua
@@ -1,6 +1,14 @@
 local util = {}
 
-local fix_up
+local fix_up,
+recipe2String,
+items2String,
+string2Attributes,
+string2CraftingCat,
+string2ItemType,
+string2Amount,
+string2Items,
+string2Recipe
 
 ---comment
 ---@param results table
@@ -66,6 +74,118 @@ function util.get_category(recipe)
         return "crafting"
     end
     return recipe.category
+end
+
+function util.get_item_type(item)
+    if item.type == nil then
+        return "item"
+    end
+    return item.type
+end
+
+function util.recipes2String(recipes)
+    local string = ""
+    for _, recipe in ipairs(recipes) do
+        string = string .. recipe2String(recipe) .. "#"
+    end
+    return string
+end
+
+function recipe2String(recipe)
+    local name           = recipe.name
+    local category       = util.get_category(recipe)
+    local results        = util.get_results(recipe)
+    local ingredients    = util.get_ingredients(recipe)
+    local string_ing     = items2String(ingredients)
+    local string_res     = items2String(results)
+    local collect_string = string.format("<%s>|<%s>|%s|%s", name, category, string_ing, string_res)
+    return collect_string
+end
+
+function items2String(data)
+    local string = ""
+    for _, item in ipairs(data) do
+        string = string .. "<" .. item.name .. ">"
+        string = string .. "<" .. item.amount .. ">"
+        string = string .. "<" .. util.get_item_type(item) .. ">"
+    end
+    return string
+end
+
+function string2Attributes(input)
+    local strings = {}
+    local count = 1
+    for match in input:gmatch("[^|]+") do
+        strings[count] = {}
+        for submatch in match:gmatch("<(.-)>+") do
+            table.insert(strings[count], submatch)
+        end
+        count = count + 1
+    end
+    return strings
+end
+
+function string2CraftingCat(string)
+    local check = false
+    check = check or string == "crafting"
+    check = check or string == "crafting-with-fluid"
+    check = check or string == "chemistry"
+    if check then
+        return string
+    else
+        error(string.format("recipe crafting category is not supported: %s", string))
+    end
+end
+
+function string2ItemType(string)
+    if string == "item" or string == "fluid" then
+        return string
+    else
+        error(string.format("item type is not supported: %s", string))
+    end
+end
+
+function string2Amount(string)
+    local num = tonumber(string)
+    if not num or num ~= math.floor(num) then
+        error(string.format("item amount is not an interger: %s", string))
+    end
+    return num
+end
+
+function string2Items(strings)
+    local temps = {}
+    for i = 1, #strings, 3 do
+        local temp = {}
+        temp.name = strings[i]
+        temp.type = string2ItemType(strings[i + 2])
+        temp.amount = string2Amount(strings[i + 1])
+        table.insert(temps, temp)
+    end
+    return temps
+end
+
+function string2Recipe(input)
+    local pack = {}
+    local subStrings = string2Attributes(input)
+    if #subStrings == 4 and #subStrings[3] % 3 == 0 and #subStrings[4] % 3 == 0 then
+        pack.name = subStrings[1][1]
+        pack.category = string2CraftingCat(subStrings[2][1])
+        pack.ingredients = string2Items(subStrings[3])
+        pack.results = string2Items(subStrings[4])
+    else
+        error("string is not formatted, correctly")
+    end
+    return pack
+end
+
+function util.string2Recipes(input)
+    local temp = {}
+    for match in input:gmatch("(.-)#") do
+        local recipe = string2Recipe(match)
+        table.insert(temp, recipe)
+    end
+    return temp
 end
 
 return util
