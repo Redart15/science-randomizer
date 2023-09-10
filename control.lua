111e5fbbbb0ef03938ceac9878e042519325acc7
Artem Betz <arbet100@uni-duesseldorf.de>
2023-09-10 10:28:51 +0200

    extracted gui element out of control

--- control.lua
+++ control.lua
@@ -1,11 +1,14 @@
-local util = require("libs.common.util")
-local lookup = require("libs.common.lookup")
-local pathprefix = "item/"
-local mod_gui = require("mod-gui")
+local lookup = require("static.randomizer-lookup")
+local create = require("script.create_gui")
+local recipes2String = require("script.recipe_serialization").recipe_serialization
 
 
-local init_globals, create_button, get_packs, initialize, toggle_textField,
-create_exportField
+local get_packs, generateString
+
+function generateString()
+    local packs = get_packs()
+    return recipes2String(packs)
+end
 
 function get_packs()
     local recipes = game.recipe_prototypes
@@ -24,56 +27,9 @@ function get_packs()
     return temp
 end
 
-function toggle_textField(player)
-    local screen_element = player.gui.screen
-    if screen_element.randomizer_export_frame == nil then
-        create_exportField(player)
-    else
-        screen_element.randomizer_export_frame.destroy()
-    end
-end
-
-function create_exportField(player)
-    local screen_element = player.gui.screen
-    local text_frame = screen_element.add {
-        type = "frame",
-        name = "randomizer_export_frame",
-        caption = { "randomizer.export" },
-    }
-    text_frame.style.size = { 405, 410 } -- lenght|height
-    text_frame.auto_center = true
-
-    player.opened = text_frame
-
-    local text_box = text_frame.add {
-        type = "text-box",
-        name = "randomizer_export_textbox",
-        text = util.recipes2String(get_packs()),
-    }
-    text_box.style.size = { 380, 300 } -- lenght|height
-    text_box.read_only = true
-    text_box.word_wrap = true
-end
-
-function create_button(player)
-    local player_globals = global.players[player.index]
-    local flow_button = mod_gui.get_button_flow(player)
-    if flow_button.randomizer_export_button then
-        flow_button.randomizer_export_button.destroy()
-    end
-    mod_gui.get_button_flow(player).add {
-        type = "sprite-button",
-        name = "randomizer_export_button",
-        sprite = pathprefix .. "automation-science-pack",
-        tooltip = "Export science recipe",
-        style = player_globals.toggle and "yellow_slot_button" or mod_gui.button_style
-    }
-    toggle_textField(player)
-end
-
-function initialize(player)
+local function initialize(player)
     global.players[player.index] = { toggle = false }
-    create_button(player)
+    create.button(player)
 end
 
 script.on_init(
@@ -86,7 +42,6 @@ script.on_init(
         global.players = {}
         for _, player in pairs(game.players) do
             initialize(player)
-            -- create_button(player)
         end
     end
 )
@@ -117,7 +72,8 @@ script.on_event(
         local player = game.get_player(event.player_index)
         local player_globals = global.players[event.player_index]
         player_globals.toggle = not player_globals.toggle
-        create_button(player)
+        create.button(player)
+        create.toggle_textField(player, generateString)
     end
 )
 
@@ -127,11 +83,12 @@ script.on_event(
         local element = event.element
         if element and element.name == "randomizer_export_button" then
             local player = game.get_player(event.player_index)
-            toggle_textField(player)
+            create.toggle_textField(player, generateString)
         end
         if element and element.name == "randomizer_export_frame" then
             local player = game.get_player(event.player_index)
             initialize(player)
+            create.toggle_textField(player, generateString)
         end
     end
 )
---
+++ script/create_gui.lua
@@ -0,0 +1,52 @@
+local mod_gui = require("mod-gui")
+local create = {}
+
+function create.exportField(player, generateString)
+    local screen_element = player.gui.screen
+    local text_frame = screen_element.add {
+        type = "frame",
+        name = "randomizer_export_frame",
+        caption = { "randomizer.export" },
+    }
+    text_frame.style.size = { 405, 410 } -- lenght|height
+    text_frame.auto_center = true
+
+    player.opened = text_frame
+
+    local text_box = text_frame.add {
+        type = "text-box",
+        name = "randomizer_export_textbox",
+        text = generateString(),
+    }
+    text_box.style.size = { 380, 300 } -- lenght|height
+    text_box.read_only = true
+    text_box.word_wrap = true
+end
+
+
+function create.button(player)
+    local player_globals = global.players[player.index]
+    local flow_button = mod_gui.get_button_flow(player)
+    if flow_button.randomizer_export_button then
+        flow_button.randomizer_export_button.destroy()
+    end
+    mod_gui.get_button_flow(player).add {
+        type = "sprite-button",
+        name = "randomizer_export_button",
+        sprite = "item/automation-science-pack",
+        tooltip = "Export science recipe",
+        style = player_globals.toggle and "yellow_slot_button" or mod_gui.button_style
+    }
+    -- create.toggle_textField(player)
+end
+
+function create.toggle_textField(player, generateString)
+    local screen_element = player.gui.screen
+    if screen_element.randomizer_export_frame == nil then
+        create.exportField(player, generateString)
+    else
+        screen_element.randomizer_export_frame.destroy()
+    end
+end
+
+return create
