d2aee9ab328ad22a442574efbef80dad151c04dc
Artem Betz <arbet100@uni-duesseldorf.de>
2023-09-26 00:13:55 +0200

    broke the gui in parts and rebuild them from the ground

--- control.lua
+++ control.lua
@@ -1,99 +1,122 @@
 local lookup = require("static.randomizer-lookup")
 local create = require("script.create_gui")
-local recipes2String = require("script.recipe_serialization").recipe_serialization
+local mod_gui = require("mod-gui")
+local recipes_serialization = require("script.recipe_serialization").recipes_serialization
 
+local serializationPacks,
+closeInterface,
+initialize,
+selectText,
+printSeed
 
-local get_packs, generateString
-
-function generateString()
-    local packs = get_packs()
-    return recipes2String(packs)
-end
-
-function get_packs()
-    local recipes = game.recipe_prototypes
-    local temp = {}
-    for key, _ in pairs(lookup["science"]) do
-        local luaRecipe = recipes[key]
-        if luaRecipe ~= nil then
-            local recipe = {}
-            recipe.name = luaRecipe.name
-            recipe.category = luaRecipe.category
-            recipe.ingredients = luaRecipe.ingredients
-            recipe.results = luaRecipe.products
-            table.insert(temp, recipe)
+function serializationPacks()
+    local function get_packs()
+        local recipes = game.recipe_prototypes
+        local temp = {}
+        for key, _ in pairs(lookup["science"]) do
+            local luaRecipe = recipes[key]
+            if luaRecipe ~= nil then
+                local recipe = {}
+                recipe.name = luaRecipe.name
+                recipe.category = luaRecipe.category
+                recipe.ingredients = luaRecipe.ingredients
+                recipe.results = luaRecipe.products
+                table.insert(temp, recipe)
+            end
         end
+        return temp
     end
-    return temp
+
+    local packs = get_packs()
+    return recipes_serialization(packs)
 end
 
-local function initialize(player)
-    global.players[player.index] = { toggle = false }
+function closeInterface(event)
+    local element = event.element
+    local closeTextfield = not element == nil
+    closeTextfield = closeTextfield and element.valid
+    closeTextfield = closeTextfield or element.name == "science_randomizer_export_button"
+    closeTextfield = closeTextfield or element.name == "science_randomizer_close_button"
+    closeTextfield = closeTextfield or element.name == "science_randomizer_confirm"
+    if closeTextfield then
+        local player = game.get_player(event.player_index)
+        create.toggle_textField(player, serializationPacks())
+        return
+    end
+end
+
+function selectText(event)
+    local element = event.element
+    local selectTextfield = element ~= nil
+    selectTextfield = selectTextfield and element.valid
+    selectTextfield = selectTextfield and element.name == "science_randomizer_select_all"
+    if selectTextfield then
+        local player_globals = global.players[event.player_index]
+        local textfield = player_globals.main_frame["science_randomizer_text_box"]
+        textfield:select_all()
+    end
+end
+
+function printSeed()
+end
+
+function initialize(player)
+    global.players[player.index] = {}
     create.button(player)
 end
 
-script.on_init(
-    function()
-        -- only here for testing
-        -- local freeplay = remote.interfaces["freeplay"]
-        -- if freeplay then -- Disable freeplay popup-message
-        --     if freeplay["set_skip_intro"] then remote.call("freeplay", "set_skip_intro", true) end
-        --     if freeplay["set_disable_crashsite"] then remote.call("freeplay", "set_disable_crashsite", true) end
-        -- end
-        global.players = {}
-        for _, player in pairs(game.players) do
-            initialize(player)
-        end
+script.on_init(function()
+    -- only here for testing
+    local freeplay = remote.interfaces["freeplay"]
+    if freeplay then -- Disable freeplay popup-message
+        if freeplay["set_skip_intro"] then remote.call("freeplay", "set_skip_intro", true) end
+        if freeplay["set_disable_crashsite"] then remote.call("freeplay", "set_disable_crashsite", true) end
     end
-)
-
-script.on_event(
-    defines.events.on_player_created,
-    function(event)
-        local player = game.get_player(event.player_index)
+    global.players = {}
+    for _, player in pairs(game.players) do
         initialize(player)
     end
-)
+end)
 
-script.on_event(
-    defines.events.on_player_joined_game,
-    function(event)
+script.on_event(defines.events.on_player_created, function(event)
+    local player = game.get_player(event.player_index)
+    initialize(player)
+end)
+
+script.on_event(defines.events.on_player_joined_game, function(event)
+    local player = game.get_player(event.player_index)
+    initialize(player)
+end)
+
+script.on_event(defines.events.on_gui_click, function(event)
+    closeInterface(event)
+    selectText(event)
+end)
+
+script.on_event(defines.events.on_gui_closed, function(event)
+    local element = event.element
+    if element and element.name == "science_randomizer_main_frame" then
         local player = game.get_player(event.player_index)
-        initialize(player)
+        create.toggle_textField(player, serializationPacks())
     end
-)
-
-script.on_event(
-    defines.events.on_gui_click,
-    function(event)
-        local element = event.element
-        if element.name ~= "randomizer_export_button" then
-            return
-        end
-        local player = game.get_player(event.player_index)
-        local player_globals = global.players[event.player_index]
-        player_globals.toggle = not player_globals.toggle
-        create.button(player)
-        create.toggle_textField(player, generateString)
-    end
-)
-
-script.on_event(
-    defines.events.on_gui_closed,
-    function(event)
-        local element = event.element
-        if element and element.name == "randomizer_export_button" then
-            local player = game.get_player(event.player_index)
-            create.toggle_textField(player, generateString)
-        end
-        if element and element.name == "randomizer_export_frame" then
-            local player = game.get_player(event.player_index)
-            initialize(player)
-            create.toggle_textField(player, generateString)
-        end
-    end
-)
+end)
 
 script.on_event(defines.events.on_player_removed, function(event)
     global.players[event.player_index] = nil
+end)
+
+script.on_configuration_changed(function(configuration_changed_data)
+    if configuration_changed_data.mod_changes["science-randomizer"] then
+        for _, player in pairs(game.players) do
+            local player_global = global.players[player.index]
+            local flow_button = mod_gui.get_button_flow(player)
+            if flow_button.randomizer_export_button then
+                flow_button.randomizer_export_button.destroy()
+                initialize(player)
+            end
+            if player_global.main_frame ~= nil then
+                create.toggle_textField(player, serializationPacks())
+            end
+        end
+    end
 end)
--- script/create_gui.lua
+++ script/create_gui.lua
@@ -1,57 +1,128 @@
 local mod_gui = require("mod-gui")
 local create = {}
 
---- export recipe sctring
----@param player LuaPlayer
----@param generateString function
-function create.exportField(player, generateString)
-    local screen_element = player.gui.screen
-    local text_frame = screen_element.add {
-        type = "frame",
-        name = "randomizer_export_frame",
-        caption = { "randomizer.export" },
+local function createTitlebar(gui)
+    local titlebar = gui.add {
+        name = "science_randomizer_titlebar",
+        type = "flow",
     }
-    text_frame.style.size = { 405, 410 } -- lenght|height
-    text_frame.auto_center = true
+    titlebar.drag_target = gui
+    titlebar.style.bottom_padding = 4
 
-    player.opened = text_frame
+    titlebar.add {
+        name = "science_randomizer_label",
+        type = "label",
+        style = "frame_title",
+        ignored_by_interaction = true,
+        caption = { "science_randomizer.export" }
+    }
 
-    local text_box = text_frame.add {
+    local filler = titlebar.add {
+        type = "empty-widget",
+        style = "draggable_space_header",
+        ignored_by_interaction = true,
+    }
+    filler.style.height = 24
+    filler.style.horizontally_stretchable = true
+    filler.style.right_margin = 4
+
+    titlebar.add {
+        name = "science_randomizer_close_button",
+        type = "sprite-button",
+        style = "frame_action_button",
+        sprite = "utility/close_white",
+        hovered_sprite = "utility/close_black",
+        clicked_sprite = "utility/close_black",
+        tooltip = { "gui.close-instruction" },
+    }
+end
+
+local function createTextfield(gui, text)
+    local text_box = gui.add {
         type = "text-box",
-        name = "randomizer_export_textbox",
-        text = generateString(),
+        name = "science_randomizer_text_box",
+        text = text,
+        style = "stretchable_textfield"
     }
-    text_box.style.size = { 380, 300 } -- lenght|height
     text_box.read_only = true
     text_box.word_wrap = true
+    text_box.style.height = 250
+    text_box.style.natural_width = 200
+end
+
+local function createFootbar(gui)
+    local buttom_flow = gui.add {
+        name = "buttom_flowt",
+        type = "flow",
+    }
+
+    buttom_flow.add {
+        type = "button",
+        name = "science_randomizer_select_all",
+        caption = { "science_randomizer.selectAll" }
+    }
+
+    local filler = buttom_flow.add {
+        type = "empty-widget",
+        ignored_by_interaction = true,
+    }
+    filler.style.height = 24
+    filler.style.horizontally_stretchable = true
+    filler.style.right_margin = 4
+
+    buttom_flow.add {
+        type = "button",
+        name = "science_randomizer_confirm",
+        caption = { "science_randomizer.confirm" }
+    }
+end
+
+function create.exportGui(player, text)
+    local player_globals = global.players[player.index]
+    local screen_element = player.gui.screen
+    local main_frame = screen_element.add {
+        name = "science_randomizer_main_frame",
+        type = "frame",
+        direction = "vertical",
+    }
+    main_frame.style.natural_width = 400
+    main_frame.auto_center = true
+    player.opened = main_frame
+
+    player_globals.main_frame = main_frame
+    createTitlebar(main_frame)
+    createTextfield(main_frame, text)
+    createFootbar(main_frame)
 end
 
 --- create the button at the top left
 ---@param player LuaPlayer
 function create.button(player)
-    local player_globals = global.players[player.index]
     local flow_button = mod_gui.get_button_flow(player)
-    if flow_button.randomizer_export_button then
-        flow_button.randomizer_export_button.destroy()
+    if flow_button["science_randomizer_export_button"] then
+        flow_button["science_randomizer_export_button"].destroy()
     end
-    mod_gui.get_button_flow(player).add {
+    local player_globals = global.players[player.index]
+    local mod_gui_button = mod_gui.get_button_flow(player).add {
         type = "sprite-button",
-        name = "randomizer_export_button",
+        name = "science_randomizer_export_button",
         sprite = "item/automation-science-pack",
         tooltip = "Export science recipe",
-        style = player_globals.toggle and "yellow_slot_button" or mod_gui.button_style
+        style = mod_gui.button_style
     }
+    player_globals.mod_gui_button = mod_gui_button
 end
 
 --- toggle the textfield
 ---@param player LuaPlayer
----@param generateString function
-function create.toggle_textField(player, generateString)
-    local screen_element = player.gui.screen
-    if screen_element.randomizer_export_frame == nil then
-        create.exportField(player, generateString)
+function create.toggle_textField(player, text)
+    local player_globals = global.players[player.index]
+    local main_frame = player_globals.main_frame
+    if main_frame == nil then
+        create.exportGui(player, text)
     else
-        screen_element.randomizer_export_frame.destroy()
+        main_frame.destroy()
+        player_globals.main_frame = nil
     end
 end
 
